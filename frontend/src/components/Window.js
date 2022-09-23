95eee99378c3cb04db684a247715093cb9fccda9
unknown <magosdk@icloud.com>
2022-09-23 12:23:54 +0200

    header window done

--- frontend/src/components/Window.js
+++ frontend/src/components/Window.js
@@ -1,14 +1,11 @@
 const Window = () => {
     return <div className='window'>
-                  <div className="catrix">
-                    <div className="title">
-                        <h1 className="uppercase">sadakhanov</h1>
-                        <h2>Magomed</h2>
+        <div className="title">
+                        <h1 className="uppercase name">sadakhanov</h1>
+                        <h2 className="name">Magomed</h2>
                         <p className="uppercase">web developer junior</p>
                     </div>
-                    {/* <img class="catrix" src={catrix} alt="catrix" width={"1030px"}/>
-                    <img class="webgenius" src={webgenius} alt="window" width={"800px"}/> */}
-                </div>
+                  <div className="catrix"></div>
            </div>
   }
   
